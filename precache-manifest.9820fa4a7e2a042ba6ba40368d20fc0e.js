c83fb06d35e3c4767d2862f26e7df5deb6fd0931
cmitchell <clmitchell630@gmail.com>
2019-11-19 20:28:44 -0600

    Updates

---
+++ precache-manifest.9820fa4a7e2a042ba6ba40368d20fc0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e5ee92b16f9ce1bd19ff4cbecebbc6",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "7eb18186288fa37a7410",
+    "url": "/clicky-game/static/css/main.35b98645.chunk.css"
+  },
+  {
+    "revision": "c7718f706d1fa03d0878",
+    "url": "/clicky-game/static/js/2.0f377ec7.chunk.js"
+  },
+  {
+    "revision": "7eb18186288fa37a7410",
+    "url": "/clicky-game/static/js/main.2861b8a3.chunk.js"
+  },
+  {
+    "revision": "166d09415de4c52327d7",
+    "url": "/clicky-game/static/js/runtime-main.cf0d8eec.js"
+  },
+  {
+    "revision": "c0864a9fc47915c685e86df172c37b55",
+    "url": "/clicky-game/static/media/congruent_pentagon.c0864a9f.png"
+  }
+]);
